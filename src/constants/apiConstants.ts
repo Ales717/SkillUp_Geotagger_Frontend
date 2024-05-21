04d73e8410dcfa5c6db04aaf89b7e85329dabfea
Ales717 <prosenjak.ales77@gmail.com>
2024-05-21 08:56:18 +0200

    first commit

---
+++ src/constants/apiConstants.ts
@@ -0,0 +1,9 @@
+export enum apiRoutes {
+  LOGIN = '/auth/login',
+  SIGNUP = '/auth/register',
+  SIGNOUT = '/auth/signout',
+  FETCH_USER = '/auth',
+  FETCH_USERS = '/users',
+  USERS_PREFIX = '/users',
+  UPLOAD_AVATAR_IMAGE = '/users/upload',
+}
